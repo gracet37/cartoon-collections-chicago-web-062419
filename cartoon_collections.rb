450fb514ba8cdf92a2bdde610d7aaf45f5af5cd9
Grace Tan <gracetan37@gmail.com>
2019-06-06 15:11:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each_with_index do |list,index|
+    puts "#{index+1} #{list}"
+end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteers)
   # Your code here
+  planeteers.map! {|name| name.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(array)
   # Your code here
+  array.any? do |calls|
+    calls.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+if string.include?(cheese_types) == true 
+  return 
