20db1b9e375b4cdf6bccffbe8dea45acaec78815
Grace Tan <gracetan37@gmail.com>
2019-06-06 15:13:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(array)
 end
 end
 
-def find_the_cheese(string)
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-string.find do |cheese|
-  string.
+cheese.find do |cheesey|
+  
